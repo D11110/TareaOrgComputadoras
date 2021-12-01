0f3ff8b44edc4e21e7a45a92b7d02349840480de
judaaaron <judaponce14@unitec.edu>
2021-12-01 13:33:03 -0600

    datos cargados a vector

--- mainCache.cpp
+++ mainCache.cpp
@@ -1,7 +1,38 @@
+#include <sstream>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <vector>
+using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+vector<int> RAM;
+void llenarRAM(vector<int>, int);
+
 
 int main(int argc, char** argv) {
+	llenarRAM(RAM,100);
+	cout<<"RAM LLenada"<<endl;
 	return 0;
 }
+
+void llenarRAM(vector<int> R, int size){
+	ifstream infile("datos.txt");
+ 	string line;
+ 	int numero;
+    while (getline(infile, line)){
+        istringstream iss(line);
+        for(int i = 0 ; i < size; i++){
+       	 if (iss >>numero)
+            R.push_back(numero);
+        }
+    }
+	cout<<R.size()<<"este es el size"<<endl;
+	for(int i = 0 ; i < size; i++){
+		cout<<R[i]<<endl;
+	}
+	cout<<endl;	
+}
+
+
+
+
